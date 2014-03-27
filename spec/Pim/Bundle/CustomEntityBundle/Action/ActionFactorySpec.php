4e9c495ec48f4d884e82b63c4eaffb64ffed40c3
Antoine Guigan <antoine@akeneo.com>
2014-03-27 19:31:28 +0100

    Added spec

--- spec/Pim/Bundle/CustomEntityBundle/Action/ActionFactorySpec.php
+++ spec/Pim/Bundle/CustomEntityBundle/Action/ActionFactorySpec.php
@@ -42,31 +42,36 @@ class ActionFactorySpec extends ObjectBehavior
         $confRegistry->get('entity2')->willReturn($configuration2);
         $confRegistry->has('entity3')->willReturn(false);
         $configuration1->hasAction('action1')->willReturn(true);
-        $configuration1->hasAction('action2')->willReturn(true);
         $configuration1->getAction('action1')->willReturn('action1');
+        $configuration1->hasAction('action2')->willReturn(true);
         $configuration1->getAction('action2')->willReturn('action2');
+        $configuration1->hasAction('action3')->willReturn(false);
         $configuration2->hasAction('action1')->willReturn(true);
         $configuration2->getAction('action1')->willReturn('action1_1');
-        $configuration2->hasAction('action2')->willReturn(false);
+        $configuration2->hasAction('action2')->willReturn(true);
+        $configuration2->getAction('action2')->willReturn('action2');
 
         $container->get('action1')->shouldBeCalledTimes(1)->willReturn($action1);
-        $container->get('action2')->shouldBeCalledTimes(1)->willReturn($action2);
+        $container->get('action2')->shouldBeCalledTimes(2)->willReturn($action2);
         $container->get('action1_1')->shouldBeCalledTimes(1)->willReturn($action1_1);
 
         $action1->setConfiguration($configuration1)->shouldBeCalledTimes(1);
         $action2->setConfiguration($configuration1)->shouldBeCalledTimes(1);
         $action1_1->setConfiguration($configuration2)->shouldBeCalledTimes(1);
+        $action2->setConfiguration($configuration2)->shouldBeCalledTimes(1);
 
         $this->getAction('entity1', 'action1')->shouldReturn($action1);
         $this->getAction('entity1', 'action2')->shouldReturn($action2);
+        $this->getAction('entity1', 'action3')->shouldReturn(null);
         $this->getAction('entity2', 'action1')->shouldReturn($action1_1);
+        $this->getAction('entity2', 'action2')->shouldReturn($action2);
         $this->getAction('entity3', 'action1')->shouldReturn(null);
-        $this->getAction('entity2', 'action2')->shouldReturn(null);
-
+        
         $this->getAction('entity1', 'action1')->shouldReturn($action1);
         $this->getAction('entity1', 'action2')->shouldReturn($action2);
+        $this->getAction('entity1', 'action3')->shouldReturn(null);
         $this->getAction('entity2', 'action1')->shouldReturn($action1_1);
+        $this->getAction('entity2', 'action2')->shouldReturn($action2);
         $this->getAction('entity3', 'action1')->shouldReturn(null);
-        $this->getAction('entity2', 'action2')->shouldReturn(null);
     }
 }
