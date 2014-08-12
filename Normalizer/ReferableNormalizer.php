6e590bce21548d98bcd86ba963bb516c09985aed
Antoine Guigan <antoine@akeneo.com>
2014-08-12 10:36:37 +0200

    Fixed normalization of custom options

---
+++ Normalizer/ReferableNormalizer.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Pim\Bundle\CustomEntityBundle\Normalizer;
+
+use Pim\Bundle\CatalogBundle\Model\ReferableInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+/**
+ * Normalizes referable entities
+ * 
+ * @author    Antoine Guigan <antoine@akeneo.com>
+ * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
+ * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+class ReferableNormalizer implements NormalizerInterface
+{
+    /**
+     * @var array
+     */
+    protected $allowedformats;
+
+    /**
+     * Constructor
+     * 
+     * @param array $allowedformats
+     */
+    function __construct(array $allowedformats)
+    {
+        $this->allowedformats = $allowedformats;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function normalize($object, $format = null, array $context = array())
+    {
+        if (array_key_exists('field_name', $context)) {
+            return [
+                $context['field_name'] => $object->getReference(),
+            ];
+        } else {
+            return $object->getReference();
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsNormalization($data, $format = null)
+    {
+        return $data instanceof ReferableInterface && in_array($format, $this->allowedformats);
+    }
+}
