d5b7d02fa80d9d4091c8c28eda09c51ef87caa4c
Romain Monceau <romain@akeneo.com>
2015-08-11 18:19:21 +0200

    Change CustomEntityRepository implementation

--- Entity/Repository/CustomEntityRepository.php
+++ Entity/Repository/CustomEntityRepository.php
@@ -3,7 +3,6 @@
 namespace Pim\Bundle\CustomEntityBundle\Entity\Repository;
 
 use Pim\Bundle\ReferenceDataBundle\Doctrine\ORM\Repository\ReferenceDataRepository;
-use Pim\Bundle\UIBundle\Entity\Repository\OptionRepositoryInterface;
 
 /**
  * Repository for the custom entity
@@ -12,46 +11,6 @@ use Pim\Bundle\UIBundle\Entity\Repository\OptionRepositoryInterface;
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class CustomEntityRepository extends ReferenceDataRepository implements OptionRepositoryInterface
+class CustomEntityRepository extends ReferenceDataRepository
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function getOption($id, $collectionId = null, array $options = array())
-    {
-        return $this->find($id);
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getOptionId($object)
-    {
-        return $object->getId();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getOptionLabel($object, $dataLocale)
-    {
-        return $object->getCode();
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getOptions($dataLocale, $collectionId = null, $search = '', array $options = array())
-    {
-        $qb = $this->createQueryBuilder('c')
-            ->select('c.id AS id, c.code AS text');
-        if ($search) {
-            $qb->andWhere(('c.code LIKE :search'))
-                ->setParameter('search', "$search%");
-        }
-
-        return array(
-            'results' => $qb->getQuery()->getArrayResult()
-        );
-    }
 }
