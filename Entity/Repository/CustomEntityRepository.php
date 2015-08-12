3249ac46238251c43361d701354a41c7ebe929f8
Romain Monceau <romain@akeneo.com>
2015-08-12 18:24:42 +0200

    Fix mass delete for custom entities

--- Entity/Repository/CustomEntityRepository.php
+++ Entity/Repository/CustomEntityRepository.php
@@ -2,6 +2,7 @@
 
 namespace Pim\Bundle\CustomEntityBundle\Entity\Repository;
 
+use Doctrine\ORM\QueryBuilder;
 use Pim\Bundle\ReferenceDataBundle\Doctrine\ORM\Repository\ReferenceDataRepository;
 
 /**
@@ -13,4 +14,66 @@ use Pim\Bundle\ReferenceDataBundle\Doctrine\ORM\Repository\ReferenceDataReposito
  */
 class CustomEntityRepository extends ReferenceDataRepository
 {
+    /**
+     * Apply mass action parameters on query builder
+     *
+     * @param QueryBuilder $qb
+     * @param bool         $inset
+     * @param array        $values
+     */
+    public function applyMassActionParameters($qb, $inset, $values)
+    {
+        if ($values) {
+            $rootAlias = $qb->getRootAlias();
+            $valueWhereCondition =
+                $inset
+                    ? $qb->expr()->in($rootAlias, $values)
+                    : $qb->expr()->notIn($rootAlias, $values);
+            $qb->andWhere($valueWhereCondition);
+        }
+
+        if (null !== $qb->getDQLPart('where')) {
+            $whereParts = $qb->getDQLPart('where')->getParts();
+            $qb->resetDQLPart('where');
+
+            foreach ($whereParts as $part) {
+                if (!is_string($part) || !strpos($part, 'entityIds')) {
+                    $qb->andWhere($part);
+                }
+            }
+        }
+
+        $qb->setParameters(
+            $qb->getParameters()->filter(
+                function ($parameter) {
+                    return $parameter->getName() !== 'entityIds';
+                }
+            )
+        );
+
+        // remove limit of the query
+        $qb->setMaxResults(null);
+    }
+
+    /**
+     * Delete a list of reference ids
+     *
+     * @param int[] $ids
+     *
+     * @return int
+     * @throws \LogicException
+     */
+    public function deleteFromIds(array $ids)
+    {
+        if (empty($ids)) {
+            throw new \LogicException('Nothing to remove');
+        }
+
+        $qb = $this->_em->createQueryBuilder();
+        $qb
+            ->delete($this->_entityName, 'ref')
+            ->where($qb->expr()->in('ref.id', $ids));
+
+        return $qb->getQuery()->execute();
+    }
 }
