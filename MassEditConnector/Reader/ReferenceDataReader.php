486057fe85bc0d542de07cec09cefa0d87d2f4d3
Romain Monceau <romain@akeneo.com>
2015-08-14 13:07:30 +0200

    CEB-9: Renaming MassEdit classes

--- MassEditConnector/Reader/ReferenceDataReader.php
+++ MassEditConnector/Reader/ReferenceDataReader.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Pim\Bundle\CustomEntityBundle\Connector\Reader\MassEdit;
+namespace Pim\Bundle\CustomEntityBundle\MassEditConnector\Reader;
 
 use Akeneo\Bundle\BatchBundle\Entity\StepExecution;
 use Akeneo\Bundle\BatchBundle\Item\AbstractConfigurableStepElement;
