fc90128b741b0d15158e6f7ee9dd76687b0f35b2
Romain Monceau <romain@akeneo.com>
2015-08-17 13:34:30 +0200

    CEB-5: Update documentation

--- Action/AbstractAction.php
+++ Action/AbstractAction.php
@@ -13,8 +13,6 @@ use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Translation\TranslatorInterface;
 
 /**
- * Abstract action
- *
  * @author    Antoine Guigan <antoine@akeneo.com>
  * @copyright 2013 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
