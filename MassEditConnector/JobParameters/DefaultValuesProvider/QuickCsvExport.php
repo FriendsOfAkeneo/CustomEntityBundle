c15608d7c1d37ea1d8e9c907ae68ecc4c2c2f295
Romain Monceau <fitn@users.noreply.github.com>
2016-08-09 12:03:12 +0200

    Update QuickCsvExport.php

--- MassEditConnector/JobParameters/DefaultValuesProvider/QuickCsvExport.php
+++ MassEditConnector/JobParameters/DefaultValuesProvider/QuickCsvExport.php
@@ -6,6 +6,8 @@ use Akeneo\Component\Batch\Job\JobInterface;
 use Akeneo\Component\Batch\Job\JobParameters\DefaultValuesProviderInterface;
 
 /**
+ * Default values provider for the quick export csv
+ * 
  * @author    Nicolas Dupont <nicolas@akeneo.com>
  * @copyright 2016 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
