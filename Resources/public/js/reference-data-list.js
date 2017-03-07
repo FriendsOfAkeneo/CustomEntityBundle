5a08c9bf54bf4ae017b8577bf267b4162f82c81a
Anael CHARDAN <anael.chardan@gmail.com>
2017-03-07 14:12:50 +0100

    Fix PR issues

--- Resources/public/js/reference-data-list.js
+++ Resources/public/js/reference-data-list.js
@@ -1,10 +1,10 @@
 'use strict';
 
 /**
- * Reference data list fetcher fetcher
+ * Reference data list fetcher
  *
- * @author    Julien Sanchez <julien@akeneo.com>
- * @copyright 2016 Akeneo SAS (http://www.akeneo.com)
+ * @author    Anaël CHARDAN <anael.chardan@akeneo.com>
+ * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 define([
