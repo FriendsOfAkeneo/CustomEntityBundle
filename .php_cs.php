7d70c3dc879c2e3037fdc670180077d9c7612bd5
jmleroux <jmleroux.pro@gmail.com>
2017-09-28 17:51:33 +0200

    2.0 migration: Add PHP CS Fixer

---
+++ .php_cs.php
@@ -0,0 +1,17 @@
+<?php
+
+return PhpCsFixer\Config::create()
+    ->setRules(array(
+        '@PSR2' => true,
+        'linebreak_after_opening_tag' => true,
+        'ordered_imports' => true,
+        'method_argument_space' => [
+            'ensure_fully_multiline' => false
+        ],
+    ))
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+            ->name('*.php')
+            ->notName('*Spec.php')
+            ->in(__DIR__)
+    );
