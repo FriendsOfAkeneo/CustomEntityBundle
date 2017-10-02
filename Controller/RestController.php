b44fd37320a4450eeac40ab0a0756a456cb44fcf
jmleroux <jmleroux.pro@gmail.com>
2017-10-02 12:46:14 +0200

    2.0 migration: PHP doc fix

--- Controller/RestController.php
+++ Controller/RestController.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
- * @author    JM Leroux <jean-marie.leroux@akeneo.com>
+ * @author    Anaël Chardan <anael.chardan@akeneo.com
  * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
