47fb8988bd97764cb86bd6560072b2448b17f6b5
jmleroux <jmleroux.pro@gmail.com>
2017-12-20 15:35:48 +0100

    CEB-53: add specs

---
+++ spec/Pim/Bundle/CustomEntityBundle/Checker/ProductLinkCheckerySpec.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace spec\Pim\Bundle\CustomEntityBundle\Checker;
+
+use Doctrine\ORM\EntityManagerInterface;
+use PhpSpec\ObjectBehavior;
+use Pim\Bundle\CustomEntityBundle\Checker\ProductLinkChecker;
+use Pim\Bundle\CustomEntityBundle\Checker\ProductLinkCheckerInterface;
+use Pim\Bundle\CustomEntityBundle\Repository\AttributeRepository;
+use Pim\Component\Catalog\Query\ProductQueryBuilderFactoryInterface;
+
+class ProductLinkCheckerSpec extends ObjectBehavior
+{
+    public function let(
+        EntityManagerInterface $em,
+        ProductQueryBuilderFactoryInterface $productQueryBuilderFactory,
+        AttributeRepository $attributeRepository
+    ) {
+        $this->beConstructedWith($em, $productQueryBuilderFactory, $attributeRepository);
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(ProductLinkChecker::class);
+        $this->shouldImplement(ProductLinkCheckerInterface::class);
+    }
+
+}
