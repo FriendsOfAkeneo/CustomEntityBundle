67e8187983062bacd00d2b7d5f4a291240e65abd
Maxime CORNET <mcornet@altima-agency.com>
2018-01-03 11:38:01 +0100

    GITHUB-130: Nullable type for getTranslation in AbstractTranslatableCustomEntity

--- Entity/AbstractTranslatableCustomEntity.php
+++ Entity/AbstractTranslatableCustomEntity.php
@@ -43,7 +43,7 @@ abstract class AbstractTranslatableCustomEntity extends AbstractCustomEntity imp
     /**
      * {@inheritdoc}
      */
-    public function getTranslation(?string $locale = null): AbstractTranslation
+    public function getTranslation(?string $locale = null): ?AbstractTranslation
     {
         $locale = ($locale) ? $locale : $this->locale;
         if (!$locale) {
